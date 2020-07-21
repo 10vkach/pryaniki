87a06115775b3e776a8b5971e21d60e1ae5d430a
Dima Zareckiy <>
2020-07-21 16:28:02 +0400

    Создан проект.

---
+++ pryaniki/pryaniki/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  pryaniki
+//
+//  Created by Dima Zareckiy on 21.07.2020.
+//  Copyright © 2020 Dima Zareckiy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
