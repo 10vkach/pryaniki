d80e60ce3cedddc0da7024ac2f1eedbb9c3b50e4
Dima Zareckiy <>
2020-07-23 14:07:03 +0400

    Добавлена структура для парса json'а

---
+++ pryaniki/pryaniki/StructsForJSON.swift
@@ -0,0 +1,118 @@
+import Foundation
+
+struct Sample: Decodable {
+    let data: [SampleData]
+    let view: [ViewStruct]
+    
+    struct SampleData: Decodable {
+        let name: String
+        let data: DataType
+        
+        enum DataType: Decodable {
+            case text(HZData)
+            case picture(PictureData)
+            case selector(SelectorData)
+            
+            init(from decoder: Decoder) throws {
+                
+                if let selectorData = SelectorData(decoder: decoder) {
+                    self = .selector(selectorData)
+                    return
+                }
+                
+                if let pictureData = PictureData(decoder: decoder) {
+                    self = .picture(pictureData)
+                    return
+                }
+                
+                if let hzData = HZData(decoder: decoder) {
+                    self = .text(hzData)
+                    return
+                }
+                
+                throw SampleError.UnknownDataType
+            }
+        }
+
+        struct HZData: Codable {
+            let text: String
+            
+            enum CodingKeys: String, CodingKey {
+                case text
+            }
+            
+            init?(decoder: Decoder) {
+                do {
+                    let container = try decoder.container(keyedBy: CodingKeys.self)
+                    text = try container.decode(String.self, forKey: .text)
+                } catch {
+                    return nil
+                }
+            }
+        }
+
+        struct PictureData: Codable {
+            let url: URL
+            let text: String
+            
+            enum CodingKeys: String, CodingKey {
+                case url, text
+            }
+            
+            init?(decoder: Decoder) {
+                do {
+                    let container = try decoder.container(keyedBy: CodingKeys.self)
+                    url = try container.decode(URL.self, forKey: .url)
+                    text = try container.decode(String.self, forKey: .text)
+                } catch {
+                    return nil
+                }
+            }
+        }
+
+        struct SelectorData: Codable {
+            let selectedId: Int
+            let variants: [Variant]
+            
+            enum CodingKeys: String, CodingKey {
+                case selectedId, variants
+            }
+            
+            struct Variant: Codable {
+                let id: Int
+                let text: String
+            }
+            
+            init?(decoder: Decoder) {
+                do {
+                    let container = try decoder.container(keyedBy: CodingKeys.self)
+                    selectedId = try container.decode(Int.self, forKey: .selectedId)
+                    variants = try container.decode([Variant].self, forKey: .variants)
+                } catch {
+                    return nil
+                }
+            }
+        }
+        
+//        enum CodingKeys: String, CodingKey {
+//            case name
+//            case data
+//        }
+//
+//        init(from decoder: Decoder) throws {
+//            let container = try decoder.container(keyedBy: CodingKeys.self)
+//            name = try container.decode(String.self, forKey: .name)
+//            data = try container.decode(DataType.self, forKey: .data)
+//        }
+        
+    }
+    
+    struct ViewStruct: Codable {
+        let item: String
+    }
+}
+
+//MARK: Errors
+enum SampleError: Error {
+    case UnknownDataType
+}
